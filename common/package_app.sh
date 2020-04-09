6716b694230cdc1161a287c3fe8f23adad3be909
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-10 00:40:24 +0200

    fixed package name

--- common/package_app.sh
+++ common/package_app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ${HOME}/oF/apps/${GH_USER}
-zip -r ${GH_REPO}.zip ${GH_REPO}
+zip -r ${GH_REPO}_${OF_PLATFORM}.zip ${GH_REPO}
 mkdir ${HOME}/packaged
-mv ${GH_REPO}.zip ${HOME}/packaged
+mv ${GH_REPO}_${OF_PLATFORM}_${RAVIS_TAG}.zip ${HOME}/packaged
