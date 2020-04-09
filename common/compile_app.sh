607cfffafc6ea12ddc015d96e8ab3f6882ff6d53
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-09 23:43:13 +0200

    cd into app

--- common/compile_app.sh
+++ common/compile_app.sh
@@ -3,6 +3,7 @@ cd ${HOME}/oF/apps
 mkdir ${GH_USER} && cd ${GH_USER}
 # git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
 cp -R ${TRAVIS_BUILD_DIR} .
+cd ${GH_REPO}
 
 bash -c "$(curl -sSL https://raw.githubusercontent.com/thomasgeissl/ofPackageManager/master/scripts/ofPackageManager.sh)" install
 make
