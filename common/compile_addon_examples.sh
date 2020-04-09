aa907606e1f0f8d770b3d2621e941cef59830063
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-09 23:49:16 +0200

    cd into addon

--- common/compile_addon_examples.sh
+++ common/compile_addon_examples.sh
@@ -2,6 +2,7 @@
 cd ${HOME}/oF/addons
 # git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
 cp -R ${TRAVIS_BUILD_DIR} .
+cd ${GH_REPO}
 
 bash -c "$(curl -sSL https://raw.githubusercontent.com/thomasgeissl/ofPackageManager/master/scripts/ofPackageManager.sh)" install
 make
