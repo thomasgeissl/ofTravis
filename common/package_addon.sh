19f69a6491fda3410d916c5d94ac5a2b4532ab7b
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-10 01:53:48 +0200

    added package addon script

---
+++ common/package_addon.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd ${HOME}/oF/addons/
+zip -r ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${GH_REPO}
+mkdir ${HOME}/packaged
+openssl dgst -sha256 ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip >> ${HOME}/packaged/${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.sha256
+mv ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${HOME}/packaged
