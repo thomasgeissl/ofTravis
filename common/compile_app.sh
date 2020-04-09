657249d99fe1a0ad043ecf5736111ab0727dd926
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-09 23:14:23 +0200

    added compile app script

---
+++ common/compile_app.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ${HOME}/oF/apps
+mkdir ${GH_USER} && cd ${GH_USER}
+# git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
+cp -R ${TRAVIS_BUILD_DIR} .
+pwd && ls
+# bash -c "$(curl -sSL https://raw.githubusercontent.com/thomasgeissl/ofPackageManager/master/scripts/ofPackageManager.sh)" install
+# make
