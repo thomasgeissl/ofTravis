78c914be16a4e1cb2902faa9e2c1d938a15a6118
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-09 23:26:11 +0200

    enabled compilation

--- common/compile_addon_examples.sh
+++ common/compile_addon_examples.sh
@@ -2,12 +2,12 @@
 cd ${HOME}/oF/addons
 # git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
 cp -R ${TRAVIS_BUILD_DIR} .
-pwd && ls
-# bash -c "$(curl -sSL https://raw.githubusercontent.com/thomasgeissl/ofPackageManager/master/scripts/ofPackageManager.sh)" install
-# make
-# for d in example*/ ; do
-#     echo "$d"
-#     cd $d
-#     make
-#     cd ..
-# done
+
+bash -c "$(curl -sSL https://raw.githubusercontent.com/thomasgeissl/ofPackageManager/master/scripts/ofPackageManager.sh)" install
+make
+for d in example*/ ; do
+    echo "$d"
+    cd $d
+    make
+    cd ..
+done
