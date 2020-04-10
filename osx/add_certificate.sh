854e8e460ddd9cd74f3f672599386539ab8e09ba
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-10 18:06:50 +0200

    added password

--- osx/add_certificate.sh
+++ osx/add_certificate.sh
@@ -4,10 +4,7 @@
   export CERTIFICATE_P12=Certificate.p12;
   echo $CERTIFICATE_OSX_P12 | base64 — decode > $CERTIFICATE_P12;
   export KEYCHAIN=build.keychain;
-#   security create-keychain -p mysecretpassword $KEYCHAIN;
-  security create-keychain $KEYCHAIN;
+  security create-keychain -p mysecretpassword $KEYCHAIN;
   security default-keychain -s $KEYCHAIN;
-#   security unlock-keychain -p mysecretpassword $KEYCHAIN;
-  security unlock-keychain $KEYCHAIN;
-#   security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
-  security import $CERTIFICATE_P12 -k $KEYCHAIN -T /usr/bin/codesign;
+  security unlock-keychain -p mysecretpassword $KEYCHAIN;
+  security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_OSX_P12_PASSWORD -T /usr/bin/codesign;
