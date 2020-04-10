07a95abe36f059f22e37f60fa8d8db6a598f5435
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-10 19:53:56 +0200

    trying to certificate name

--- osx/sign_addon_examples.sh
+++ osx/sign_addon_examples.sh
@@ -3,6 +3,6 @@ cd ${HOME}/oF/addons/${GH_REPO}
 for d in example*/ ; do
     echo "$d"
     cd $d
-    codesign --force --sign "Developer ID Application: My Company" ./bin/*.app
+    codesign --force --sign "Thomas Geissl" ./bin/*.app
     cd ..
 done
