2b39988d0a380908fb02e3cdcb8dc56601f9e21e
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-10 13:24:55 +0200

    silently compresssing

--- common/package_app.sh
+++ common/package_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ${HOME}/oF/apps/${GH_USER}
-zip -r ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${GH_REPO}
+zip -qq -r ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${GH_REPO}
 mkdir ${HOME}/packaged
 openssl dgst -sha256 ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip >> ${HOME}/packaged/${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.sha256
 mv ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${HOME}/packaged
