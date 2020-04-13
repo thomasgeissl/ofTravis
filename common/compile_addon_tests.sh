f9d5a5f89bcd0327e79a9053206f64f21ee2a513
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-13 14:24:17 +0200

    added script to compile addons tests

--- common/compile_addon_tests.sh
+++ common/compile_addon_tests.sh
@@ -3,7 +3,7 @@ cd ${HOME}/oF/addons
 cp -R ${TRAVIS_BUILD_DIR} .
 cd ${GH_REPO}
 
-for d in example*/ ; do
+for d in test*/ ; do
     echo "$d"
     cd $d
     bash -c "$(curl -sSL https://raw.githubusercontent.com/thomasgeissl/ofPackageManager/master/scripts/ofPackageManager.sh)" install
