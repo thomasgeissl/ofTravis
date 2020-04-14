a39c6acfa2c25df098be62fbc0bfe0fabde98dae
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-14 02:59:51 +0200

    some debug logs

--- osx/add_certificate.sh
+++ osx/add_certificate.sh
@@ -20,5 +20,22 @@ security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_OSX_P12_PASSWORD 
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
 
+KEY_CHAIN=ios-build.keychain
+security create-keychain -p travis $KEY_CHAIN
+# Make the keychain the default so identities are found
+security default-keychain -s $KEY_CHAIN
+# Unlock the keychain
+security unlock-keychain -p travis $KEY_CHAIN
+# Set keychain locking timeout to 3600 seconds
+security set-keychain-settings -t 3600 -u $KEY_CHAIN
+
+echo "list keychains: "
+security list-keychains
+echo " ****** "
+
+echo "find indentities keychains: "
+security find-identity -p codesigning  ~/Library/Keychains/$KEY_CHAIN
+echo " ****** "
+
 # remove certs
 rm -fr *.p12
