66e63f14c0eab743d46d40073055c2b5c6e52509
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-17 00:18:34 +0200

    remove git dir

--- common/package_app.sh
+++ common/package_app.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd ${HOME}/oF/apps/${GH_USER}
-zip -qq -r ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${GH_REPO}
+cp -r ${GH_REPO} package
+rm -rf package/.git
+# TODO: further clean up, e.g. obj
+zip -qq -r ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip package
 mkdir ${HOME}/packaged
 openssl dgst -sha256 ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip >> ${HOME}/packaged/${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.sha256
 mv ${GH_REPO}_${OF_PLATFORM}_${TRAVIS_TAG}.zip ${HOME}/packaged
